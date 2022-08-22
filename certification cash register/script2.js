0146289b9b9f57baeb410903da8d7982ab13db21
RchOne <wlricardo1@gmail.com>
2022-08-22 18:48:09 +0200

    It's actually day 36

--- certification cash register/script2.js
+++ certification cash register/script2.js
@@ -15,7 +15,7 @@ function checkCashRegister(price, cash, cid) {
     console.log(returnValue);
     return returnValue;
 }
-
+//
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], 
 ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], 
 ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
