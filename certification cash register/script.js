28a2ea56686860547950e2ef908d26055c1887d2
RchOne <wlricardo1@gmail.com>
2022-08-24 17:12:33 +0200

    Day 38. Challenge complete, 1 more to go

--- certification cash register/script.js
+++ certification cash register/script.js
@@ -1,50 +1,67 @@
 function checkCashRegister(price, cash, cid) {
     let changeAmmount=cash-price;
     let returnValue={
-        "status" : "OPEN",
+        "status" : "",
         "change": []
     };
-    
-    let bills = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01]; //bills and coins
-    let quotes = [`"ONE HUNDRED"`, `"TWENTY"`, `"TEN"`, `"FIVE"`, `"ONE"`, `"QUARTER"`, `"DIME"`, `"NICKEL"`, `"PENNY"`];
-    //yes, I could bills and quotes into a class, but I already have to change stuff
-    
+    let currency={
+        "value":[100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01],
+        "name":["ONE HUNDRED", "TWENTY", "TEN", "FIVE", "ONE", "QUARTER",
+        "DIME", "NICKEL", "PENNY"]
+    };
+    let totalCash=0;
     for(let i=0;i<=8;i++){
+        totalCash+= cid[i][1];
+    }
+    if(totalCash<changeAmmount){
+        returnValue.status= "INSUFFICIENT_FUNDS";
+        return returnValue;
+    }
+    if(totalCash==changeAmmount){
+        returnValue.status="CLOSED";
+        returnValue.change=cid;
+        console.log(returnValue);
+        return returnValue;
+    }
+    for(let i=0; i<=8;i++){     //and this is for OPEN
         let firstCall=0;
-        while(changeAmmount>=bills[i] && cid[8-i][1]!=0){  
-            changeAmmount -= bills[i];
-            cid [8-i][1] -= bills[i];
+        while((changeAmmount+0.00001)>= currency.value[i] && cid[8-i][1]>0){ //float aproximation BS
+            changeAmmount -= currency.value[i];
+            cid [8-i][1] -= currency.value[i];
             if(firstCall==0){
-                returnValue.change.push([quotes[i], Number(bills[i])]);
+                returnValue.change.push([currency.name[i], currency.value[i]]);
                 firstCall=1;
             }
             else{
-                returnValue.change[returnValue.change.length-1][1]+=Number(bills[i]);
+                returnValue.change[returnValue.change.length-1][1] += currency.value[i];
             }
         }
     }
-    if(changeAmmount>0){
+    if(changeAmmount>0.00001){  //float aproximation BS
         returnValue.status= "INSUFFICIENT_FUNDS";
+        returnValue.change=[];
     }
-    else {
-        for(let i=0;i<=8;i++){
-            if(cid[i][1]==0){
-                returnValue.status = "CLOSED";
-            }else{
-                returnValue.status = "OPEN" ;
-                break;
-            }
-        }
+    else{
+        returnValue.status = "OPEN";
     }
-    /*
-    returnValue.change.push(quotes[5], bills[2]);
-    returnValue.change[returnValue.change.length-1]+=bills[1];*/
-    console.log(typeof(returnValue.change[1]) )
     return returnValue;
 }
+/*
+checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], 
+["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], 
+["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+*/
 
-let testOutput= checkCashRegister(20, 28, [["PENNY", 10], ["NICKEL", 10], 
-["DIME", 10], ["QUARTER", 0], ["ONE", 0], ["FIVE", 5], 
-["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 100]]);
+// 5
 
-console.log(testOutput);
+checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], 
+["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], 
+["TWENTY", 0], ["ONE HUNDRED", 0]])
+
+
+// 6
+/*
+checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], 
+["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], 
+["TWENTY", 0], ["ONE HUNDRED", 0]])
+*/
