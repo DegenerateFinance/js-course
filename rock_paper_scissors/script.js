0ad4c30cc597730a2d8b80fd065d43c401a82d79
RchOne <wlricardo1@gmail.com>
2022-09-10 19:53:28 +0200

    Let's actually build something

---
+++ rock_paper_scissors/script.js
@@ -0,0 +1,10 @@
+const userChoiceDisplay = document.getElementById("user-choice");
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+
+let userChoice;
+let computerChoice;
+let result
+
+
