39f32becca8419844580a070cd09d27b8e66d9b1
RchOne <wlricardo1@gmail.com>
2022-09-11 11:54:52 +0200

    Day 45, js and html interaction. Get element by ...

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -2,9 +2,14 @@ const userChoiceDisplay = document.getElementById("user-choice");
 const computerChoiceDisplay = document.getElementById('computer-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
+var rock = document.getElementById("rock");
 
 let userChoice;
 let computerChoice;
 let result
 
+rock.addEventListener("click", myFunction);
 
+function myFunction() {
+  alert ("Hello World!");
+}
